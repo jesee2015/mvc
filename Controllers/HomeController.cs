cbd5383357dd7ea2029abbd2f59f1a27c7c61965
柳曦 <shakelx@qq.com>
2017-09-20 22:38:53 +0800

    add ModelBinder

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,26 @@
+﻿using MvcAppDemo.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MvcAppDemo.Controllers
+{
+    public class HomeController : Controller
+    {
+        //
+        // GET: /Home/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult UpdateAuction(Auction auction)
+        {
+            return Json(auction);
+        }
+
+    }
+}
---
+++ Models/Auction.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MvcAppDemo.Models
+{
+    public class Auction
+    {
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public decimal Price { get; set; }
+        public DateTime StartTime { get; set; }
+        public DateTime EndTime { get; set; }
+
+        public List<Bid> Bids { get; set; }
+    }
+
+    public class Bid
+    {
+        public DateTime TimeStamp { get; set; }
+        public decimal Amount { get; set; }
+    }
+}
